2bc75590ac21c6d1eeee82a8517c46e563d9d972
JALOWE <unconfigured@null.spigotmc.org>
2018-09-16 10:43:57 +1000

    based the new attributes based upon dictionary from csv

--- src/parse_csv.py
+++ src/parse_csv.py
@@ -1,10 +1,13 @@
 import csv
 import sys
+import json
 
 try:
   arg_year = sys.argv[1]
 except:
   arg_year = '2017'
+
+a = dict()
 
 with open('../data/GODT_downloads.csv', 'rt') as csvfile:
   reader = csv.DictReader(csvfile)
@@ -17,7 +20,21 @@ with open('../data/GODT_downloads.csv', 'rt') as csvfile:
       except:
         percentage = 'N/A'
       print(line['ISO'],line['REPORTYEAR'],line['TOTAL Actual DD'],line['Actual DBD'],line['Actual DCD'],line['Total Heart'],percentage)
-      
+      a.update({line['ISO'] : {'Second': 2, 'Third': 3, 'First': 1}})
+
+with open('../countries.geo.json', 'rt') as jsonfile:
+  geo = json.load(jsonfile)
+  print(geo['features'])
+  for o in geo['features']:
+    print(o['id'],o['properties'])
+    try:
+      b = a[o['id']]
+      o['properties'].update(b)
+    except:
+      print('not found')
+  with open('../research.geo.json', 'wt') as researchfile:
+    json.dump(geo,researchfile)
+
 #  for row in spamreader:
 #    i
 #    print(', '.join(row))
